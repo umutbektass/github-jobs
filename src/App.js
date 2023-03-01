5488700760789ec64e6a3c7b3adae1ea272ff836
Umut Bektaş <umutbektass.78@hotmail.com>
2023-03-02 00:37:07 +0300

    jobs website

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
+import './style.css'
 import './App.css';
+import jobs from './json/jobs/jobs.json'
+import { FaGlobeEurope } from 'react-icons/fa';
+import Filters from './component/Filters'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+      <header>
+        <h2>Github <span>Jobs</span></h2>
+        <section>
+          <div className="header-form">
+            <input placeholder="title"></input>
+            <button className="btn btn-primary">Search</button>
+          </div>
+        </section>
       </header>
+
+      {/* main */}
+
+      <main>
+      <div className="flex-items">
+        <div className="column-1">
+          
+            <Filters></Filters>
+          
+        </div>
+        <div className="column-2">
+         
+          {
+             
+            jobs.map(u=>(
+              <div className="card">
+            <div className="jobs">
+              <h5>{u.şirket}</h5>
+              <h2>{u.title}</h2>
+              <div className="card-down">
+              <div className="jobs-time">
+              
+              {
+                
+                u.time.map(time=>(
+                  
+                  <span>{time}</span>
+                  )
+                )
+               
+              }
+              </div>
+              <div className="jobs-country">
+              <span><FaGlobeEurope/></span>
+               {
+                  u.lokasyon.map(u=>(
+                   
+                    <h7>{u.ülke}</h7>
+                  ))
+                }
+                </div>
+                </div>
+
+            </div>
+
+               
+            </div> )
+              )
+          }
+          </div>
+          
+        
+       
+      </div>
+
+      </main>
+     
     </div>
   );
 }
---
+++ src/component/Filters.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import { FaGlobeEurope } from 'react-icons/fa';
+import '../style.css'
+import { useFormik ,Field,Formik,Form} from 'formik';
+
+function Filters() {
+    
+        const formik = useFormik({
+            initialValues:{
+                time:"",
+                search:"",
+                country:"",
+                color:[]
+            },
+            onSubmit:values=>{
+                console.log(values)
+            }
+        })
+    
+  return (
+    <div className="filters">
+    <Formik
+      initialValues={{
+        time:[],
+        country:"",
+        search:""
+        
+      }}
+      onSubmit={async (values) => {
+        console.log(values)
+      }}
+    >
+    {({ values }) => (
+        <Form>  
+            <div className="filters">
+             <div>   <label>
+            <Field type="checkbox" value="full" name="time" className="form-check-input"></Field>Full Time</label></div>
+           <div><label><Field type="checkbox" value="part" name="time" className="form-check-input"></Field>Part Time</label> </div> 
+            <div><label><Field type="checkbox" value="remote" name="time" className="form-check-input"></Field>Remote</label></div> 
+            <div className="search">
+                <label>LOCATION</label>
+                <Field type="text" name="search" placeholder="City,state,zip code or country"></Field>
+            </div>
+
+            <div className="country">
+            <div>   <label>
+            <Field type="radio" value="istanbul" name="country" className="form-check-input"></Field>İstanbul</label></div>
+           <div><label><Field type="radio" value="kastamonu" name="country" className="form-check-input"></Field>Kastamonu</label> </div> 
+            <div><label><Field type="radio" value="karabuk" name="country" className="form-check-input"></Field>Karabük</label>
+            </div>
+            <div><label><Field type="radio" value="hatay" name="country" className="form-check-input"></Field>Hatay</label>
+            </div>
+            </div>
+
+            <button className="btn btn-primary " type="submit">Uygula</button></div>
+        
+        </Form>
+      )}
+    </Formik>
+    </div>
+  )
+}
+
+export default Filters
